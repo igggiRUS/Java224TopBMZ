f5c9405eb82f4f1ab3e68b7c34521d8eea40ad3f
MikeBezhaneishvili <mike215misha@gmail.com>
2022-06-05 17:36:38 +0300

    Двенадцатая задача решена

--- src/hw02/task03.java
+++ src/hw02/task03.java
@@ -2,9 +2,12 @@ package  hw02;
 
 public class task03 {
 	public static void main(String[] args) {
-		double x = 3.9327819;
+		double x = 3.93456456279;
+		// почему программа предложила сделать приведение числа?
+		double result = Math.round(x);
 
-		System.out.println("округление до целого:" + (int)x); // округление спомощью явного приведения
 
+		System.out.println("Округление до целого: " + result); // округление спомощью явного приведения
+		System.out.println("Округление до целого с явным приведением прведением : " + ((int) result)); // округление спомощью явного приведения
 	}
 }
--- src/hw02/test.java
+++
@@ -1,7 +0,0 @@
-package hw02;
-
-public class test {
-	public static void main(String[] args) {
-
-	}
-}
