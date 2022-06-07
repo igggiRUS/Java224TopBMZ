bc646f914ed9612df03575dc5f7624283847b2c8
MikeBezhaneishvili <mike215misha@gmail.com>
2022-06-07 08:27:38 +0300

    5w03

--- src/hw03/task05.java
+++ src/hw03/task05.java
@@ -1,5 +1,27 @@
 package hw03;
 
-public class task05 {
+import java.util.Scanner;
 
+public class task05 {
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter: ");
+	if (sc.hasNextInt()) {
+		int first = sc.nextInt();
+		 
+		System.out.println("enter second number");
+		if (sc.hasNextInt()) {
+			int second = sc.nextInt();
+			
+			System.out.println("enterOperation ");
+			char operation = sc.next().charAt(0);
+			
+			int result;
+			if (operation == '-')
+		}
+	}
+		
+		
+	}
 }
---
+++ src/test/www.java
@@ -0,0 +1,13 @@
+package test;
+
+public class www {
+	public static void main(String[] args) {
+
+		boolean z = true;
+		String biString = Integer.toBinaryString(z);
+
+		
+		System.out.println(biString);
+
+	}
+}
