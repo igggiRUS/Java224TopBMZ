c077199bfa94621920d5df73d9c3c25db4f79932
MikeBezhaneishvili <mike215misha@gmail.com>
2022-06-13 23:30:10 +0300

    заметки после урока

--- src/hw02/task09.java
+++ src/hw02/task09.java
@@ -4,8 +4,17 @@ package hw02;
 
 public class task09 {
 	public static void main(String[] args) {
-		double i = 34.145;
-		System.out.println(i);
+		double q = 34.145;
+		double w = 2.5;
+		double e = 10.0;
+		double r = 5.0;
+		System.out.println(q % 1);
+		System.out.println(w % 1);
+		System.out.println(e % 1);
+		System.out.println(r % 1);
 	
 	}
 }
+
+//
+// Может я не прав но вещественные это те у которых остался остаток после нуля.
